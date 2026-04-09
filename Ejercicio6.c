fcf39da01cce9581270157f059df60bf49c09860
fridaquintana <fridasq90@gmail.com>
2026-04-09 14:26:50 -0600

    Create Ejercicio6.c

---
+++ Ejercicio6.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#define MAX 5
+int main ()
+{
+  int arreglo[MAX], cont;
+  for (cont=0; cont<MAX; cont++)
+  {
+    arreglo[MAX], cont;
+    for (cont=0; cont<MAX; cont++)
+  }
+  printf("El valor ingresado para cada elemento del arreglo es:\n[");
+  for (cont=0; cont<MAX; cont++)
+  {
+      printf("%d\t", arreglo[cont]);
+  }
+  printf("]\n");
+  return 0;
+}
